d423a49fb0c952912494ff202dd8f4b64e58d6b7
Dipanjan Bhowmik <onlydevelop@gmail.com>
2018-07-04 09:20:35 +0530

    Initial commit of ItemService.

---
+++ src/main/java/com/restaurant/itemservice/Item.java
@@ -0,0 +1,39 @@
+package com.restaurant.itemservice;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+	
+	@Id
+	private long id;
+	
+	private String name;
+	private int price;
+	
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public Item() {}
+	
+	public Item(long id, String name, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+}
---
+++ src/main/java/com/restaurant/itemservice/ItemsController.java
@@ -0,0 +1,32 @@
+package com.restaurant.itemservice;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@Autowired
+	private ItemRepository repository;
+	
+	@GetMapping("/items/{id}/type/{type}")
+	public Item getItem(@PathVariable Long id, @PathVariable String type) {
+		
+		Optional<Item> item = repository.findById(id);
+		return getCalculatedItem(item.get(), type); 
+	}
+	
+	private Item getCalculatedItem(Item item, String type) {
+		double factor = 1.0;
+		if (type.equals("restaurant")) factor = configuration.getFactor();
+		item.setPrice((int) (item.getPrice() * factor));
+		return item;
+	}
+}
